b5919075c62a8b058e1e0d14e87ad261fc31f34e
Antoine Garcia <a.garcia.walecha@gmail.com>
2023-03-04 13:18:04 +0100

    add all users

--- pages/me.tsx
+++ pages/me.tsx
@@ -1,12 +1,16 @@
 import { useSession } from "next-auth/react"
 import Layout from "../components/layout"
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
-export default function MePage() {
+export default async function MePage() {
   const { data } = useSession()
+  const allUsers = await prisma.user.findMany()
 
   return (
     <Layout>
       <h1>This is me</h1>
+      <pre>{JSON.stringify(allUsers, null, 2)}</pre>
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </Layout>
   )
